da4032dbb528b2cdf4634df6d4dbaef448eaf3a1
github username <github email address>
2019-09-12 14:27:17 -0500

    commitment!

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,32 +1,45 @@
-import React, { Component } from 'react'
-import TransactionsList from './TransactionsList'
-import Search from './Search'
-import {transactions} from '../transactionsData'
+import React, { Component } from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+// import { transactions } from "../transactionsData";
 
 class AccountContainer extends Component {
-
   constructor() {
-    super()
-
-    // get a default state working with the data imported from TransactionsData
-    // use this to get the functionality working
-    // then replace the default transactions with a call to the API
-
+    super();
+    this.state = {
+      transactions: [],
+      searchQuery: ""
+    };
   }
 
-  handleChange(event) {
-    // your code here
+  handleChange = event => {
+    this.setState({ searchQuery: event.target.value });
+  };
+
+  componentDidMount() {
+    fetch("https://boiling-brook-94902.herokuapp.com/transactions")
+      .then(resp => resp.json())
+      .then(json => this.setState({ transactions: json }));
   }
+
+  // I made this case-agnostic for easier use
+  filterTransactionsByCategory = () => {
+    return this.state.searchQuery === ""
+      ? this.state.transactions
+      : this.state.transactions.filter(transaction => {
+          const category = transaction.category.toLowerCase();
+          return category.includes(this.state.searchQuery.toLowerCase());
+        });
+  };
 
   render() {
-
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} />
+        <TransactionsList transactions={this.filterTransactionsByCategory()} />
       </div>
-    )
+    );
   }
 }
 
-export default AccountContainer
+export default AccountContainer;
